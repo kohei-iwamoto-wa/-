1b5cdfba0488734f816c0a0ad9681e95224dc6ef
kohei-iwamoto-wa <bananadesu9@gmail.com>
2021-01-28 00:13:44 +0900

    PWA化ファイル追加

---
+++ serviceworker.js
@@ -0,0 +1,46 @@
+const cacheName = 'converterShell';
+
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/book.js',
+    '/styles/style.css',
+    '/images/book.png'
+];
+
+self.addEventListener('install', function(event) {
+    console.log('Service Worker installing');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function(cache){
+                console.log('Service Worker caching app shell');
+                return cache.addAll(filesToCache);
+        })
+    )
+});
+
+self.addEventListener('active', function(event){
+    console.log('ServerWorker Active');
+    event.waitUntil(
+        caches.keys()
+            .then(function(keyList){
+                return Promise.all(keyList.map(function(key){
+                if (key !== cacheName) {
+                    console.log('Service Worker removing old cache', key);
+                    return caches.delete(key);
+                }
+            }))
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+    console.log('ServerWorker Fetch', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            return response || fetch(event.request);
+        })
+    );
+});
